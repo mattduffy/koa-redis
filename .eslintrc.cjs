b8037aaed57c1a98672e95fc6839ca36a632fc5d
Matthew Duffy <mattduffy@gmail.com>
2026-01-09 19:38:58 -0800

    Add eslint-import-resolver-exports package for imporved linting.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,47 @@
+const restrictedGlobals = require('eslint-restricted-globals')
+
 module.exports = {
+  settings: {
+    'import/resolver': {
+      exports: {},
+      node: {
+        extensions: ['.js', '.mjs'],
+      },
+    },
+  },
+  globals: {
+    window: true,
+    document: true,
+    origin: true,
+    worker: true,
+  },
   env: {
     es2021: true,
     node: true,
+    browser: true,
   },
   plugins: [
   ],
   extends: 'airbnb-base',
   overrides: [
+    {
+      files: ['public/j/worker.js'],
+      rules: {
+        'no-restricted-globals': ['error', 'isFinite', 'isNaN'].concat(restrictedGlobals),
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    'no-use-before-define': ['error', {
+      functions: false,
+      classes: false,
+      variables: true,
+      allowNamedExports: true,
+    }],
     semi: ['error', 'never'],
     'no-console': 'off',
     'no-underscore-dangle': 'off',
